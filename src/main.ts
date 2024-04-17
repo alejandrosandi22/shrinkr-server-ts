68cf4333b2bd5611a9f2e6959392b664dba9c9a6
Alejandro Sandí <alesf22@gmail.com>
2024-04-17 10:15:00 -0600

    chore: initial source code

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.setGlobalPrefix('api/v1');
+  app.enableCors({
+    origin: process.env.CLIENT_APP_URL,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(5000);
+}
+bootstrap();
