18029e1dd53feec78b77d249df7d167ff3876859
Alejandro Sandí <alesf22@gmail.com>
2024-04-17 19:34:52 -0600

    chore: set environment variables types

---
+++ src/types/environment.d.ts
@@ -0,0 +1,27 @@
+export {};
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      ENV: 'test' | 'dev' | 'prod';
+      DB_HOST: string;
+      DB_PORT: number;
+      DB_USER: string;
+      DB_PASSWORD: string;
+      DB_NAME: string;
+
+      JWT_SECRET: string;
+
+      FACEBOOK_CLIENT_ID: string;
+      FACEBOOK_CLIENT_SECRET: string;
+      FACEBOOK_CALLBACK_URL: string;
+
+      GOOGLE_CLIENT_ID: string;
+      GOOGLE_CLIENT_SECRET: string;
+      GOOGLE_CALLBACK_URL: string;
+
+      CLIENT_APP_URL: string;
+      SERVER_APP_URL: string;
+    }
+  }
+}
