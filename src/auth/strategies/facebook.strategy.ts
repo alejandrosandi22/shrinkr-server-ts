c07e683a5b1061b14bf17177b2b6f009564a66b5
Alejandro Sandí <alesf22@gmail.com>
2024-04-18 10:17:02 -0600

    feat: create authentication module

---
+++ src/auth/strategies/facebook.strategy.ts
@@ -0,0 +1,36 @@
+import { ProviderEnum } from '@/lib/enums/provider.enum';
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Profile, Strategy } from 'passport-facebook';
+
+@Injectable()
+export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
+  constructor() {
+    super({
+      clientID: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+      scope: 'email',
+      profileFields: ['emails', 'name', 'photos'],
+    });
+  }
+
+  async validate(
+    _accessToken: string,
+    _refreshToken: string,
+    profile: Profile,
+    done: (error: any, user?: any, info?: any) => void,
+  ): Promise<any> {
+    const { id, name, emails, photos } = profile;
+
+    const user = {
+      provider: ProviderEnum.FACEBOOK,
+      providerId: id,
+      email: emails[0].value,
+      name: `${name.givenName} ${name.familyName}`,
+      avatar: photos[0].value,
+    };
+
+    done(null, user);
+  }
+}
