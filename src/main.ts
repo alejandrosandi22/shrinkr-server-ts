55b403206825e687821971b119aec6ff7d7cd2b3
Alejandro Sandí <alesf22@gmail.com>
2024-04-29 11:59:55 -0600

    feat: remove express device

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import * as device from 'express-device';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.setGlobalPrefix('api/v1');
-  app.use(device.capture());
   app.enableCors({
     origin: process.env.CLIENT_APP_URL,
   });
