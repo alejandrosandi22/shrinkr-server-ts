3bdc49d2e7789efaf815ef2879972bb867f5f320
Alejandro Sandí <alesf22@gmail.com>
2024-05-05 11:28:57 -0600

    refactor: get analytics data in one request

--- src/analytics/analytics.controller.ts
+++ src/analytics/analytics.controller.ts
@@ -5,42 +5,9 @@ import { AnalyticsService } from './analytics.service';
 export class AnalyticsController {
   constructor(private readonly analyticsService: AnalyticsService) {}
 
-  @Get('main-stats/:id')
-  async findMainStats(@Param('id') id: string) {
-    const uniqueVisitors =
-      await this.analyticsService.countUniqueVisitsLastThirtyDays(+id);
-    const visits = await this.analyticsService.countVisitsLastThirtyDays(+id);
-    const topCountry =
-      await this.analyticsService.getTopCountryOfLastThirtyDays(+id);
-    const topReferrer =
-      await this.analyticsService.getTopReferrerOfLastThirtyDays(+id);
-
-    return {
-      uniqueVisitors,
-      visits,
-      topCountry,
-      topReferrer,
-    };
-  }
-
-  @Get('top-devices/:id')
-  async getTopDevicesOfLastThirtyDays(@Param('id') id: string) {
-    return await this.analyticsService.getTopDevicesOfLastThirtyDays(+id);
-  }
-
-  @Get('top-platforms/:id')
-  async getTopPlatformsOfLastThirtyDays(@Param('id') id: string) {
-    return await this.analyticsService.getTopPlatformsOfLastThirtyDays(+id);
-  }
-
-  @Get('top-referrers/:id')
-  async getTopReferrersOfLastThirtyDays(@Param('id') id: string) {
-    return await this.analyticsService.getTopReferrersOfLastThirtyDays(+id);
-  }
-
-  @Get('top-countries/:id')
-  async getTopCountriesOfLastThirtyDays(@Param('id') id: string) {
-    return await this.analyticsService.getTopCountriesOfLastThirtyDays(+id);
+  @Get('overview/:id')
+  getPerformanceOfLastThirtyDays(@Param('id') id: string) {
+    return this.analyticsService.getPerformanceOfLastThirtyDays(+id);
   }
 
   @Get('url-analytics/:url')
