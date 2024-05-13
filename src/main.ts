4d2748f1bd2a73350a6b847bb7177433a725b367
Alejandro Sandí <alesf22@gmail.com>
2024-05-12 21:51:10 -0600

    feat: use compression and log memory usage

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as compression from 'compression';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,6 +8,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api/v1');
   app.enableCors();
+  app.use(compression());
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,6 +17,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const formatMemoryUsage = (data) =>
+    `${Math.round((data / 1024 / 1024) * 100) / 100} MB`;
+  const memoryData = process.memoryUsage();
+  const memoryUsage = {
+    rss: `${formatMemoryUsage(memoryData.rss)} -> Resident Set Size - total memory allocated for the process execution`,
+    heapTotal: `${formatMemoryUsage(memoryData.heapTotal)} -> total size of the heap`,
+    heapUsed: `${formatMemoryUsage(memoryData.heapUsed)} -> actual memory used`,
+  };
+  console.log(memoryUsage);
 
   await app.listen(parseInt(process.env.PORT) || 5000);
 }
